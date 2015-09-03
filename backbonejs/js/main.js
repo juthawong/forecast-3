c78c90c48b09d36e7633baac9ca78396925f8e6a
Lukasz Adamczuk <lukasz.jakub.adamczuk@gmail.com>
2015-09-03 15:23:40 +0200

    starting BackboneJS version

---
+++ backbonejs/js/main.js
@@ -0,0 +1,9 @@
+console.log('main logic is ready...');
+
+(function($){
+  // Object declarations goes here
+  $(document).ready(function () {
+    // Start application code goes here
+    // ...
+  });
+})(jQuery);
